1cbd1db644b6e20d90e4d8257a934a203b8dcd9e
dao <dev.di.yszw@gmail.com>
2024-08-03 21:20:20 +0900

    Update Japanese translation for pydantic.

--- tests/check_usage_docs.py
+++ tests/check_usage_docs.py
@@ -12,31 +12,33 @@ version_file = PYDANTIC_DIR / 'version.py'
 
 
 version = re.search(rb"VERSION = '(.*)'", version_file.read_bytes()).group(1)
-version_major_minor = b'.'.join(version.split(b'.')[:2])
-expected_base = b'https://docs.pydantic.dev/' + version_major_minor + b'/'
+# version_major_minor = b'.'.join(version.split(b'.')[:2])
+# expected_base = b'https://docs.pydantic.dev/' + version_major_minor + b'/'
+version_major_minor = b'pydantic-docs-ja'
+expected_base = b'https://yodai-yodai.github.io/translated' + version_major_minor + b'/'
 
 paths = sys.argv[1:]
 error_count = 0
-# for path_str in paths:
-#     path = ROOT_DIR / path_str
-#     b = path.read_bytes()
+for path_str in paths:
+    path = ROOT_DIR / path_str
+    b = path.read_bytes()
 
-#     changed = 0
+    changed = 0
 
-#     def sub(m: re.Match) -> bytes:
-#         global changed
-#         if m.group(2) != expected_base:
-#             changed += 1
-#             return m.group(1) + expected_base
-#         else:
-#             return m.group(0)
+    def sub(m: re.Match) -> bytes:
+        global changed
+        if m.group(2) != expected_base:
+            changed += 1
+            return m.group(1) + expected_base
+        else:
+            return m.group(0)
 
-#     b = re.sub(rb'(""" *usage.docs: *)(https://.+?/.+?/)', sub, b, flags=re.I)
-#     if changed:
-#         error_count += changed
-#         path.write_bytes(b)
-#         plural = 's' if changed > 1 else ''
-#         print(f'{path_str:50} {changed} usage docs link{plural} updated')
+    b = re.sub(rb'(""" *usage.docs: *)(https://.+?/.+?/.+?/)', sub, b, flags=re.I)
+    if changed:
+        error_count += changed
+        path.write_bytes(b)
+        plural = 's' if changed > 1 else ''
+        print(f'{path_str:50} {changed} usage docs link{plural} updated')
 
 if error_count:
     sys.exit(1)
